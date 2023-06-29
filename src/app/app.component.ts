6263c44b00631c23f2988126b6e04903f9ac877f
kasztancode <adamczajkowski970@gmail.com>
2023-06-30 00:47:25 +0200

    heh half way there

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  audioContext: AudioContext;
-  audioBuffer!: AudioBuffer;
-  audioSource!: AudioBufferSourceNode;
-  durationInSeconds!: number;
-  isPlaying: boolean;
-  startTime: number;
-  elapsedTime: number;
+export class AppComponent implements AfterViewInit {
+  @ViewChild('audioPlayer') audioPlayer!: ElementRef<HTMLAudioElement>;
+  @ViewChild('progressbar', { static: false }) progressbar!: ElementRef;
 
-  constructor() {
-    this.audioContext = new AudioContext();
-    this.loadAudioFile();
-    this.isPlaying = false;
-    this.startTime = 0;
-    this.elapsedTime = 0;
+  isPlaying: boolean = false;
+  progressPercent: number = 0;
+  volume: number = 0.2;
+  durationInSeconds: number = 0;
+  timer: any;
+  isInsideDiv: boolean = false;
+  progressLeftX!: number;
+  progressRightX!: number;
+  highlightedTime: number = 0;
+  highlightedPoint!: number;
+  viewportWidth!: number;
+  viewportHeight!: number;
+  highlightedPercent!:number
+  isProgressClicked:boolean = false
+
+  ngAfterViewInit() {
+    this.audioPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+      this.durationInSeconds = this.audioPlayer.nativeElement.duration;
+      this.findProgressEdges();
+    });
   }
 
-  loadAudioFile() {
-    const audioUrl = './assets/skrryt.mp3'; // Replace with the path to your song file
-    fetch(audioUrl)
-      .then(response => response.arrayBuffer())
-      .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
-      .then(audioBuffer => {
-        this.audioBuffer = audioBuffer;
-        this.durationInSeconds = audioBuffer.duration;
-        console.log('Song duration:', this.durationInSeconds, 'seconds');
-      })
-      .catch(error => {
-        console.error('Error loading audio file:', error);
-      });
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event): void {
+    this.viewportWidth = window.innerWidth;
+    this.viewportHeight = window.innerHeight;
+    this.findProgressEdges();
+  }
+
+  findProgressEdges() {
+    const progressBarElement: HTMLElement = this.progressbar.nativeElement;
+    const rect: DOMRect = progressBarElement.getBoundingClientRect();
+    this.progressLeftX = rect.left;
+    this.progressRightX = rect.right;
+  }
+
+  onProgressClick() {
+    this.highlightedPercent = (this.highlightedPoint - this.progressLeftX) / (this.progressRightX - this.progressLeftX)*100;
+    this.isProgressClicked = true
+    this.audioPlayer.nativeElement.currentTime = this.highlightedTime;
+  }
+
+  onMouseEnter(event: MouseEvent) {
+    this.isInsideDiv = true;
+  }
+
+  onMouseLeave(event: MouseEvent) {
+    this.isInsideDiv = false;
+  }
+
+  onMouseMove(event: MouseEvent) {
+    if (this.isInsideDiv) {
+      this.highlightedPoint = event.clientX;
+      this.highlightedTime = this.durationInSeconds * (this.highlightedPoint - this.progressLeftX) / (this.progressRightX - this.progressLeftX);
+    }
   }
 
   play() {
-    if (this.isPlaying) {
-      return; // Do nothing if already playing
-    }
-
-    this.audioSource = this.audioContext.createBufferSource();
-    this.audioSource.buffer = this.audioBuffer;
-    this.audioSource.connect(this.audioContext.destination);
-    this.audioSource.start(0, this.elapsedTime); // Start from the elapsed time
-    this.startTime = this.audioContext.currentTime - this.elapsedTime;
+    this.audioPlayer.nativeElement.play();
     this.isPlaying = true;
+    this.startTimer();
+    this.audioPlayer.nativeElement.volume = this.volume;
   }
 
   pause() {
-    if (!this.isPlaying) {
-      return; // Do nothing if not playing
-    }
-
-    this.audioSource.stop();
-    this.audioSource.disconnect();
-    this.elapsedTime = this.audioContext.currentTime - this.startTime;
+    this.audioPlayer.nativeElement.pause();
     this.isPlaying = false;
+    this.stopTimer();
+  }
+
+  togglePlay() {
+    if (this.isPlaying) {
+      this.pause();
+    } else {
+      this.play();
+    }
+  }
+
+  startTimer() {
+    this.progressPercent = (this.audioPlayer.nativeElement.currentTime / this.durationInSeconds) * 100;
+    this.timer = setInterval(() => {
+      this.progressPercent = (this.audioPlayer.nativeElement.currentTime / this.durationInSeconds) * 100;
+      if (Math.floor(this.audioPlayer.nativeElement.currentTime) === Math.floor(this.durationInSeconds)) {
+        this.stopTimer();
+      }
+      this.isProgressClicked=false
+    }, 10);
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+  }
+
+  changeVolume() {
+    this.audioPlayer.nativeElement.volume = this.volume;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SecondsToMinutesPipe } from './seconds-to-minutes.pipe';
+import {NgOptimizedImage} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondsToMinutesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgOptimizedImage,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

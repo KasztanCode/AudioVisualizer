e2b7987abff87e3737d6ec100d409d58deb064f0
4damc <4dam.cz@gmail.com>
2023-07-04 11:57:05 +0200

    made circular audioVisualizer and tweaked some stuff

---
+++ src/app/seconds-to-minutes.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'secondsToMinutes',
+})
+export class SecondsToMinutesPipe implements PipeTransform {
+  transform(value: number): string {
+    const flooredValue: number = Math.floor(value);
+    const minutes: number = Math.floor(flooredValue / 60);
+    const seconds: number = flooredValue % 60;
+    const formattedMinutes: string = this.padNumber(minutes);
+    const formattedSeconds: string = this.padNumber(seconds);
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
+
+  private padNumber(number: number): string {
+    return number.toString().padStart(2, '0');
+  }
+}
